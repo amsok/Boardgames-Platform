e487936f18191b7cc445513894a174e05474a6e1
= <38375497+amsok@users.noreply.github.com>
2021-10-26 09:39:36 +0200

    Sql connection
    Modified Startup with sql connection

--- Boardgames Plaform/Startup.cs
+++ Boardgames Plaform/Startup.cs
@@ -1,6 +1,8 @@
+using Boardgames_Plaform.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +26,12 @@ namespace Boardgames_Plaform
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<Boardgames_PlatformContext>(
+
+                options => options.UseSqlServer(Configuration.GetConnectionString("Boardgames"))
+           );
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -55,3 +62,4 @@ namespace Boardgames_Plaform
         }
     }
 }
+
